5c3508cc17ab057a6339b5e42e8471b3884e43ad
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-07 11:03:00 +0300

    add aws log insights query to check exceptions

--- aws/log_insights/queries.sql
+++ aws/log_insights/queries.sql
@@ -4,3 +4,11 @@ fields @timestamp, @message
 | filter @message like /ERROR/
 | sort @timestamp desc
 | limit 20
+
+-- Get a number of exceptions per hour
+filter @message like /Exception/
+| stats count(*) as exceptionCount by bin(1h)
+| sort exceptionCount desc
+
+-- Get not Exceprions log entries
+fields @message | filter @message not like /Exception/
